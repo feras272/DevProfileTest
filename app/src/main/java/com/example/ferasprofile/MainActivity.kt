0c7789cdd1d3d8d6e8f98e7bde10816d290f04a1
feras272 <ferasdev@gmail.com>
2020-04-26 07:13:14 +0300

    I made a taplet profile for portable and landscape and made a new color in xml file

--- app/src/main/java/com/example/ferasprofile/MainActivity.kt
+++ app/src/main/java/com/example/ferasprofile/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.ferasprofile
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.round
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bmap = BitmapFactory.decodeResource(resources,R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources,bmap)
+        rounded.cornerRadius = 15f
+        //rounded.isCircular = true
+
+        logo.setImageDrawable(rounded)
     }
 }
